ed4c9e1c268138e8dbf13546a9fe0a19405226e8
PPKunOfficial <2967630235@qq.com>
2026-07-25 23:25:05 +0800

    添加 Rust 语言支持，创建 .gitignore、launch.json 和 settings.json 文件

---
+++ leetcode/rust/src/bin/p0001.rs
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=rust
+ *
+ * [1] 两数之和
+ */
+
+// 本地 rust-analyzer 需要该类型；LeetCode 只提交 code start/end 之间的代码。
+pub struct Solution;
+
+// @lc code=start
+use std::collections::HashMap;
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+        nums.into_iter()
+            .enumerate()
+            .find_map(|(i, num)| {
+                let c = target - num;
+                if let Some(&prev_idx) = map.get(&c) {
+                    Some(vec![prev_idx as i32, i as i32])
+                } else {
+                    map.insert(num, i as i32);
+                    None
+                }
+            })
+            .unwrap()
+    }
+}
+// @lc code=end
+
+fn main() {}
