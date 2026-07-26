10a98270cd3729bc85cf63f68dad99fd0f68ebb4
PPKunOfficial <2967630235@qq.com>
2026-07-26 14:04:52 +0800

    [1470] 重新排列数组 rust

---
+++ leetcode/rust/src/bin/p1470.rs
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=1470 lang=rust
+ *
+ * [1470] 重新排列数组
+ */
+pub struct Solution;
+// @lc code=start
+impl Solution {
+    pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+        let n = n as usize;
+
+        nums[..n]
+            .iter()
+            .zip(&nums[n..])
+            .flat_map(|(&x, &y)| [x, y])
+            .collect()
+    }
+}
+// @lc code=end
+
+fn main() {}
