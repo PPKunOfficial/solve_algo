ae9d8007f427f1fdd0561a7920ccc286ec8ffee9
PPKunOfficial <2967630235@qq.com>
2026-07-26 14:04:48 +0800

    [1929] 数组串联 rust

---
+++ leetcode/rust/src/bin/p1929.rs
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=1929 lang=rust
+ *
+ * [1929] 数组串联
+ */
+
+pub struct Solution;
+
+// @lc code=start
+impl Solution {
+    pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
+        nums.repeat(2)
+    }
+}
+// @lc code=end
+
+fn main() {}
